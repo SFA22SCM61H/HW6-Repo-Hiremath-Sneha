70c5960792cc666d0993015889394cb435939fe9
SFA22SCM61H <shiremath2+cs587@hawk.iit.edu>
2022-11-14 11:43:40 -0600

    flast test

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
-from flask import send_file
-
+from flask import Flask,Response,jsonify, render_template ,logging,request
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+#run server
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
